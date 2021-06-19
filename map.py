a3757932398748c62c67a45d8b5bd1893066c9f4
zendo <p_geondo@snu.ac.kr>
2021-06-19 17:18:41 +0900

    feat: separate mapInfo as txt file

--- map.py
+++ map.py
@@ -2,14 +2,6 @@ from tkinter import *
 from unit import Unit
 from tower import Tower
 
-mapData = [
-    2,
-    "0 450 200 400 600 500 900 375 300 225 600 100 1000 200 1200 150",
-    "0 1170 150 1 500 375 1 820 520 1 730 240 1 370 80",
-    "0 450 200 400 600 500 900 375 300 225 600 100 1000 200 1200 150",
-    "0 100 150 1 500 375 2 820 520 1 730 240 1 370 80",
-]
-
 
 class Map(Canvas):
     def __init__(self, parent, *args, **kwargs):
@@ -21,29 +13,27 @@ class Map(Canvas):
 
         self.roadInfo = []
         self.towerInfo = []
-        for i in range(mapData[0]):
-            self.roadInfo.append(map(int, mapData[2 * i + 1].split()))
-            self.towerInfo.append([*map(int, mapData[2 * i + 2].split())])
-
+        # mapData: # of stage \n roadInfo \n towerInfo \n ...
+        with open("./mapData.txt", "r") as f:
+            for i in range(int(f.readline())):
+                self.roadInfo.append([*map(int, f.readline().split())])
+                self.towerInfo.append([*map(int, f.readline().split())])
         # road
         self.create_line(*self.roadInfo[0], width=50, capstyle=ROUND, fill="#613613")
         # tower
         for i in range(0, len(self.towerInfo[0]), 3):
             tower = self.towerInfo[0]
             self.towerList.append(Tower(self, tower[i], (tower[i + 1], tower[i + 2])))
-
         self.bind("<Button-1>", self.getXY)  # for tower initialization
 
     def addUnit(self, unitId):
-        self.unitList.append(Unit(self, unitId))
+        self.unitList.append(Unit(self, unitId, self.roadInfo[self.stage - 1]))
 
     def nextFrame(self):
+        self.after(20, self.nextFrame)
         for unit in self.unitList:
             unit.update()
-        for tower in self.towerList:
-            tower.update()
         self.parent.moneyLabel.configure(text=self.parent.money)
-        self.after(20, self.nextFrame)
 
     def getXY(self, e):  # for tower initialization
         print(e)
